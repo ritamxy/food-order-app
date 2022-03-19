651eb05d86d4a56c8ecd96e6c8623cd5422d5b2f
ritamxy <58613546+ritamxy@users.noreply.github.com>
2022-03-20 00:42:26 +1100

    Reading Form Values

--- src/components/Carts/Checkout.js
+++ src/components/Carts/Checkout.js
@@ -1,31 +1,45 @@
+import { useRef } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefaut();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
   };
+
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input tyep="text" id="name" />
+        <input tyep="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input tyep="text" id="street" />
+        <input tyep="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input tyep="text" id="postal" />
+        <input tyep="text" id="postal" ref={postalCodeInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input tyep="text" id="city" />
+        <input tyep="text" id="city" ref={cityInputRef} />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
